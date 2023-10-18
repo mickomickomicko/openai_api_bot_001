db72e991090f711c4ea7684ee44ae1e1d82417c3
mickomickomicko <55340569+mickomickomicko@users.noreply.github.com>
2023-10-18 11:54:44 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -23,10 +23,10 @@ def communicate():
       messages=messages
       )
 
-      bot_message = response["choices"][0]["message"]
-      messages.append(bot_message)
+    bot_message = response["choices"][0]["message"]
+    messages.append(bot_message)
 
-      st.session_state[user_input] = "" # 入力欄を削除
+    st.session_state[user_input] = "" # 入力欄を削除
 
 
 # ユーザーインターファイスの構築
