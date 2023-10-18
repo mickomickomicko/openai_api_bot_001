02edf65500e00c985695538347db8d3cbd801604
mickomickomicko <55340569+mickomickomicko@users.noreply.github.com>
2023-10-18 14:39:52 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def communicate():
     response = openai.ChatCompletion.create(
       model="gpt-3.5-turbo",
       messages=messages
-      )
+    )
 
     bot_message = response["choices"][0]["message"]
     messages.append(bot_message)
